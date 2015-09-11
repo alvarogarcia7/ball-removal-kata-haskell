5e8036526178974972c57171a8dd09d8da586012
Alvaro Garcia <derecerca@gmail.com>
2015-09-11 11:32:59 +0200

    green: parse direction from a string

--- Spec.hs
+++ Spec.hs
@@ -9,12 +9,21 @@ main = do
         it "should be green" $ do
           True `shouldBe` True
 
+    describe "toDirection" $ do
+        it "parse a string" $ do
+	  toDirection "123<<>>..3" `shouldBe` [N,N,N,L,L,R,R,N,N,N]
+
 property_true :: Bool -> Bool
 property_true _ = True
 
 data Direction = L | R | N deriving (Show, Eq)
 
+toDirection :: String -> [Direction]
+toDirection = map parseDirection
+
 parseDirection :: Char -> Direction
 parseDirection '<' = L
 parseDirection '>' = R
 parseDirection x = N
+
+
