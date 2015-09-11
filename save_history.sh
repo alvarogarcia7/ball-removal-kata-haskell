1d8585f4dfa7c32778e8825910edb9154efde088
Alvaro Garcia <derecerca@gmail.com>
2015-09-11 11:29:07 +0200

    defect: variable in save history should not be expanded

--- save_history.sh
+++ save_history.sh
@@ -1,1 +1,1 @@
-cat C:\Users\Technical2015\AppData\Roaming/ghc/ghci_history > .tmp && cat ghci_history >> .tmp && mv .tmp ghci_history
+cat $APPDATA/ghc/ghci_history > .tmp && cat ghci_history >> .tmp && mv .tmp ghci_history
